abca69e7df15ef9aa8628de81451218710649c51
Mateo <mateo.perez.suarez@opendeusto.es>
2024-10-06 11:54:10 +0200

    script dataChecking y prediccion age

--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import pandas as pd
-import numpy as np
-from  matplotlib import pyplot as plt
-from sklearn import decomposition as PCA
-from sklearn.preprocessing import  StandardScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.decomposition import PCA
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
-bank_Data = pd.read_csv('Churn_Modelling.csv')
-bank_Data.fillna(value=0)
-#print(bank_Data)
-
-#Valores nulos 
-
-dataLimpio = bank_Data.dropna()
-dataLimpio = dataLimpio.drop_duplicates()
-
-#Contar Repetidos
-print(sum(dataLimpio.duplicated()))
-print(dataLimpio.isnull().sum())
+data = pd.read_csv('Churn_Modelling.csv')
 
 
-columnasRecom = ['Age','CreditScore', 'EstimatedSalary','Balance']
-x = dataLimpio.loc[:,columnasRecom]
-x = StandardScaler().fit_transform(x)
-standarization = pd.DataFrame(data = x, columns = columnasRecom)
-print(standarization)
+numeric_cols = ['CreditScore', 'Balance', 'Tenure', 'NumOfProducts', 'HasCrCard', 'IsActiveMember', 'EstimatedSalary']
+categorical_cols = ['Geography', 'Gender']
 
-#PCA
-pca = PCA(n_components=2)
-principalComponents = pca.fit_transform(x)
-pcDataframe = pd.DataFrame(data = principalComponents, columns=['principal comp 1', 'principal comp 2'] )
-print(pcDataframe)
+#As we have some rows with NaN values we should not take them into account for a proper working of the function
+data_train = data.dropna(subset=numeric_cols + categorical_cols + ['Age'])
+
+X = data_train[numeric_cols + categorical_cols]
+y = data_train['Age']
+
+#Need to obtain the dummy variables for both of the categorical columns that we are using
+X = pd.get_dummies(X, columns=categorical_cols, drop_first=True)
+
+
+model = LinearRegression()
+model.fit(X, y)
+
+missing_Value = data[data['Age'].isna()][numeric_cols + categorical_cols]
+missing_Value = pd.get_dummies(missing_Value, columns=categorical_cols, drop_first=True)
+missing_Value = missing_Value.reindex(columns=X.columns, fill_value=0)
+
+age_predicted = model.predict(missing_Value)
+print(age_predicted[0])
+data.loc[data['Age'].isna(), 'Age'] = age_predicted[0]
+
+
+
+
+
