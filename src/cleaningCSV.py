c7d9d0158f0a607514bda794929648ccae963bf6
Mateo <mateo.perez.suarez@opendeusto.es>
2024-10-19 14:24:08 +0200

    Delete Cols

--- src/cleaningCSV.py
+++ src/cleaningCSV.py
@@ -3,8 +3,13 @@ import pandas as pd
 df = pd.read_csv('src/rawDataset.csv')
 print(df.head())
 
-dummy_columns = ['Gender','Geography','Card Type']
+'''Columns rowNumber and Surname are not useful to train our models in next stages of the projects so we have decided to delete them.
+Surname is not necessary as we have a column ClientID which is the unique ID from each of the customers, so another identification column is not necessary for us'''
 
+df.drop(columns=['RowNumber','Surname'], inplace= True)
+
+
+dummy_columns = ['Gender','Geography','Card Type']
 for col in dummy_columns:
     if df[col].nunique()  > 2:
         df_dummies = pd.factorize(df[col])[0]
