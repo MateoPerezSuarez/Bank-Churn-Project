61dea5645fa4f76421f72d8404c1264a7efbb365
Mateo <mateo.perez.suarez@opendeusto.es>
2024-10-23 17:59:19 +0200

    normal dataset

--- dataExploration/dataChecking.py
+++ dataExploration/dataChecking.py
@@ -3,7 +3,7 @@ import numpy as np
 import seaborn as sns
 from  matplotlib import pyplot as plt
 
-df = pd.read_csv("Bank-Churn-Project/src/cleanDataset.csv")
+df = pd.read_csv("src/cleanDataset.csv")
 
 '''Comprobations to check if there are any missing values or repeated columns'''
 print(df.isna().sum())
@@ -13,6 +13,7 @@ print(f"There are: {df.duplicated().sum()} duplicated rows")
 '''Explanation:
     We are going to make some plotting tasks to analyze our dataset,
     to have a clear overview of our dataset, such as: Distribution of data, if it is balanced or not,...'''
+    
 plt.figure(figsize= (5,5))
 plot_data = (df[['Exited']].value_counts())
 plt.pie(plot_data, labels=['Not exited', 'Exited'], autopct='%1.1f%%', startangle=90)
@@ -26,6 +27,8 @@ plt.show()
     BUSCAR SOLUTIONES FUTURAS
     
     '''
+
+'''Corre'''
 corr_matrix = df[['CreditScore', 'Age', 'Tenure', 'Balance', 'NumOfProducts', 'EstimatedSalary']].corr()
 sns.heatmap(corr_matrix, annot=True, cmap= "Spectral")
 plt.show()
--- src/pruebas.py
+++ src/pruebas.py
@@ -6,9 +6,13 @@ from sklearn import preprocessing
 df = pd.read_csv('src\cleanDataset.csv')
 print(df.head())
 
+columnas = df.columns
 x = df.values
 scaler = preprocessing.MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
-df = pd.DataFrame(x_scaled)
+df = pd.DataFrame(x_scaled, columns = columnas)
 
 print(df.head())
+df.to_csv('src/normalDataset.csv', index = False)
+
+
