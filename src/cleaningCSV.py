e4bdb771f3a93b7c3e1277ad22f048963b0d425e
Mateo <mateo.perez.suarez@opendeusto.es>
2024-10-24 13:23:17 +0200

    corr+piechart

--- src/cleaningCSV.py
+++ src/cleaningCSV.py
@@ -1,30 +1,32 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import LabelEncoder
 df = pd.read_csv('src/rawDataset.csv')
 print(df.head())
 
 '''Columns rowNumber, Surname and CustomerId are not useful to train our models in next stages of the projects so we have decided to delete them.
 Surname is not necessary as we have a column ClientID which is the unique ID from each of the customers, so another identification column is not necessary for us'''
 
-df.drop(columns=['RowNumber','Surname','CustomerId'], inplace= True)
-
-
-dummy_columns = ['Gender','Geography','Card Type']
-for col in dummy_columns:
-        df_dummies = pd.factorize(df[col])[0]
-        df[col] = df_dummies
-
-
-print(df.head())
-df.to_csv('src/cleanDataset.csv', index = False)
-
-
+'''OPCION 1'''
+columns_to_change = df.select_dtypes(include = "object").columns
+print(columns_to_change)
+'''
+encoder = OneHotEncoder(drop= "first")
+x = encoder.fit_transform(df[columns_to_change]).toarray()
+x = pd.DataFrame(x, columns = encoder.get_feature_names_out())
+x = x.astype(int)
+temp = df.drop(columns_to_change, axis=1)
+df = pd.concat([temp,x], axis=1)
+'''
 # import category_encoders as ce
 # encoder = ce.TargetEncoder(cols=['nombre_de_la_variable'])
 # df['nombre_de_la_variable_encoded'] = encoder.fit_transform(df['nombre_de_la_variable'], df['target'])
 
 
 
+
+print(df.head())
+df.to_csv('src/cleanDataset.csv', index = False)
 
 
 
--- src/pruebas.py
+++ src/pruebas.py
@@ -1,10 +1,13 @@
 import pandas as pd
+import numpy as np
+
 from sklearn import preprocessing
-
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
 '''We have'''
-df = pd.read_csv('src\cleanDataset.csv')
-print(df.head())
+df = pd.read_csv('src/rawDataset.csv')
+df.drop(columns=['RowNumber','Surname','CustomerId'], inplace= True)
+
 
 columnas = df.columns
 x = df.values
@@ -12,7 +15,32 @@ scaler = preprocessing.MinMaxScaler()
 x_scaled = scaler.fit_transform(x)
 df = pd.DataFrame(x_scaled, columns = columnas)
 
+
+
 print(df.head())
 df.to_csv('src/normalDataset.csv', index = False)
 
+
+'''OPCION 1'''
+columns_to_change = df.select_dtypes(include = "object").columns
+print(columns_to_change)
+'''
+encoder = OneHotEncoder(drop= "first")
+x = encoder.fit_transform(df[columns_to_change]).toarray()
+x = pd.DataFrame(x, columns = encoder.get_feature_names_out())
+x = x.astype(int)
+temp = df.drop(columns_to_change, axis=1)
+df = pd.concat([temp,x], axis=1)
+'''
+
+'''OPCION 2
+
+encoder = LabelEncoder()
+
+for col in columns_to_change:
+    df[col] = encoder.fit_transform(df[col])
+    print(f"Column {col} changed successfully")
+
+print(df.head())
+'''
 
